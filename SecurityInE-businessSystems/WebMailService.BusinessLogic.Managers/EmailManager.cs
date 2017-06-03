dc35265a49546eb8c562d764aabcf17af8274e78
bozobjekovic <bozo.bjekovic@gmail.com>
2017-06-04 00:00:38 +0200

    Added business logic and default template for web mail service

---
+++ SecurityInE-businessSystems/WebMailService.BusinessLogic.Managers/EmailManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebMailService.Model;
+using WebMailService.Repository;
+using WebMailService.Repository.DB;
+
+namespace WebMailService.BusinessLogic.Managers
+{
+    public class EmailManager : IEmailManager
+    {
+        private IEmail emailDBRepository = new EmailRepository();
+
+        public void ComposeEmail(Email email, List<Guid> senderAndReceiversIDs)
+        {
+            emailDBRepository.ComposeEmail(email, senderAndReceiversIDs);
+        }
+
+        public void DeleteEmail(Guid emailID)
+        {
+            emailDBRepository.DeleteEmail(emailID);
+        }
+
+        public ICollection<Email> GetInbox(User user)
+        {
+            return emailDBRepository.GetInbox(user);
+        }
+
+        public ICollection<Email> GetSent(User user)
+        {
+            return emailDBRepository.GetSent(user);
+        }
+
+        public ICollection<Email> GetTrash(User user)
+        {
+            return emailDBRepository.GetTrash(user);
+        }
+
+        public Email MoveToTrash(Guid emailID)
+        {
+            return emailDBRepository.MoveToTrash(emailID);
+        }
+    }
+}
---
+++ SecurityInE-businessSystems/WebMailService.BusinessLogic/IEmailManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebMailService.Model;
+
+namespace WebMailService.BusinessLogic
+{
+    public interface IEmailManager
+    {
+        void ComposeEmail(Email email, List<Guid> senderAndReceiversIDs);
+        ICollection<Email> GetInbox(User user);
+        ICollection<Email> GetSent(User user);
+        ICollection<Email> GetTrash(User user);
+        Email MoveToTrash(Guid emailID);
+        void DeleteEmail(Guid emailID);
+    }
+}
