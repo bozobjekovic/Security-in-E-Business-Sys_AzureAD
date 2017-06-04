bd8e88fe79388352795c84cdd3889555fbc2d99e
bozobjekovic <bozo.bjekovic@gmail.com>
2017-06-04 19:40:05 +0200

    Fixed some bugs and fixed user's name on star page

---
+++ SecurityInE-businessSystems/WebMailService/Helpers/UserDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+
+namespace WebMailService.Helpers
+{
+    public static class UserDetails
+    {
+        public static string GetUserDetails()
+        {
+            ClaimsPrincipal cp = ClaimsPrincipal.Current;
+
+            if (cp != null)
+            {
+                return string.Format("Hello {0} {1}", cp.FindFirst(ClaimTypes.GivenName).Value, cp.FindFirst(ClaimTypes.Surname).Value);
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
